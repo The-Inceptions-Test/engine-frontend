79cbf2e3bed773b1aa93974bfc14ad49b09e94cb
The-Inceptions <83852285+The-Inceptions@users.noreply.github.com>
2023-10-23 03:09:19 -0400

    able to unmarshal OAM assets + add it in event data

--- go.mod
+++ go.mod
@@ -4,8 +4,9 @@ go 1.21.1
 
 require (
 	github.com/google/uuid v1.3.1
-	github.com/owasp-amass/config v0.1.5
-	github.com/owasp-amass/open-asset-model v0.2.0
+	github.com/owasp-amass/config v0.1.6-0.20231023070030-9acd19954ee0
+	github.com/owasp-amass/engine v0.0.0
+	github.com/owasp-amass/open-asset-model v0.2.1-0.20231013010615-d39e43d5ff8b
 )
 
 require (
@@ -14,10 +15,11 @@ require (
 	github.com/VividCortex/gohistogram v1.0.0 // indirect
 	github.com/andybalholm/cascadia v1.3.2 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
-	github.com/caffix/stringset v0.1.1 // indirect
+	github.com/caffix/queue v0.1.5 // indirect
+	github.com/caffix/stringset v0.1.2 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
-	github.com/chromedp/cdproto v0.0.0-20230914224007-a15a36ccbc2e // indirect
-	github.com/chromedp/chromedp v0.9.2 // indirect
+	github.com/chromedp/cdproto v0.0.0-20231019002500-864b42864d36 // indirect
+	github.com/chromedp/chromedp v0.9.3 // indirect
 	github.com/chromedp/sysutil v1.0.0 // indirect
 	github.com/dgraph-io/badger v1.6.2 // indirect
 	github.com/dgraph-io/ristretto v0.1.1 // indirect
@@ -31,19 +33,21 @@ require (
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
-	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
+	github.com/matttproud/golang_protobuf_extensions/v2 v2.0.0 // indirect
 	github.com/owasp-amass/amass/v4 v4.2.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/prometheus/client_golang v1.17.0 // indirect
 	github.com/prometheus/client_model v0.5.0 // indirect
-	github.com/prometheus/common v0.44.0 // indirect
+	github.com/prometheus/common v0.45.0 // indirect
 	github.com/prometheus/procfs v0.12.0 // indirect
 	github.com/temoto/robotstxt v1.1.2 // indirect
 	github.com/tylertreat/BoomFilters v0.0.0-20210315201527-1a82519a3e43 // indirect
-	golang.org/x/net v0.15.0 // indirect
-	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
+
+replace github.com/owasp-amass/engine => ../engine
--- gqlclient/adems_code.go
+++ gqlclient/adems_code.go
@@ -7,12 +7,18 @@ import (
 	"log"
 	"net"
 	"net/netip"
+	"strconv"
 
 	"github.com/google/uuid"
 	"github.com/owasp-amass/config/config"
+	"github.com/owasp-amass/engine/events"
 	oam "github.com/owasp-amass/open-asset-model"
+	oamContact "github.com/owasp-amass/open-asset-model/contact"
 	fqdn "github.com/owasp-amass/open-asset-model/domain"
 	oamNet "github.com/owasp-amass/open-asset-model/network"
+	oamOrg "github.com/owasp-amass/open-asset-model/org"
+	oamPeople "github.com/owasp-amass/open-asset-model/people"
+	oamWHOIS "github.com/owasp-amass/open-asset-model/whois"
 )
 
 // Constants to represent the IPv4 and IPv6 types.
@@ -23,7 +29,11 @@ const (
 
 // TODO:
 // PUT THE REQUEST STRUCT AND ASSET STRUCT SO THE CLIENT AND SERVER DOES NOT HAVE DUOPLICATED CODE
-// FIX THE UNMARSHALLING OF THE ASSET STRUCT
+// Send data to event scheduler & process it. I already have a function to put data into the event struct
+// We just need to process it.
+
+// please just look at my code and see if you can make it better and utilize it for your needs.
+// I am not a go expert.
 
 // Request struct to hold the configuration details.
 type Request struct {
@@ -32,8 +42,12 @@ type Request struct {
 
 // Asset struct to hold the details about an asset.
 type Asset struct {
-	Session  uuid.UUID     `json:"session_id,omitempty"`
-	Event    string        `json:"event_name,omitempty"`
+	Session uuid.UUID `json:"session_id,omitempty"`
+	Event   string    `json:"event_name,omitempty"`
+	Data    AssetData `json:"data,omitempty"`
+}
+
+type AssetData struct {
 	OAMAsset oam.Asset     `json:"asset"`
 	OAMType  oam.AssetType `json:"type"`
 }
@@ -43,18 +57,15 @@ func adems_code_for_main_func() {
 	cf := flag.String("cf", "", "config file to use")
 	flag.Parse()
 
-	// Create a new configuration instance.
-	testConf := config.NewConfig()
-
-	// Acquire the configuration from the specified file.
-	err := config.AcquireConfig("", *cf, testConf)
-	if err != nil {
-		fmt.Println(err) // Handle any errors that occur during configuration acquisition.
-	}
-
 	// Populate the Request struct with the acquired configuration.
 	Request := Request{
-		Config: testConf,
+		Config: config.NewConfig(),
+	}
+
+	// Acquire the configuration from the specified file.
+	err := config.AcquireConfig("", *cf, Request.Config)
+	if err != nil {
+		fmt.Println(err) // Handle any errors that occur during configuration acquisition.
 	}
 
 	// Convert the Request struct to a JSON object.
@@ -63,12 +74,12 @@ func adems_code_for_main_func() {
 		log.Fatalf("Error occurred during marshalling: %s", err) // Handle errors that occur during JSON marshalling.
 	}
 
-	// Convert the JSON bytes to a string and print it.
+	// **DEBUG TO SHOW THE JSON STRING**
 	jsonString := string(sessionJSON)
-	fmt.Println(jsonString) // **DEBUG TO SHOW THE JSON STRING**
+	fmt.Println(jsonString)
 
 	// ***DEBUG*** Unmarshal the JSON string back into a Request object and handle any errors.
-	// fmt.Println("Config address: ", &testConf, "\n")
+	// fmt.Println("Config address: ", &Request.Config, "\n")
 	// request, err := UnmarshalRequest(jsonString)
 	// if err != nil {
 	// 	log.Fatalf("Error unmarshalling Request: %s", err)
@@ -78,27 +89,59 @@ func adems_code_for_main_func() {
 	// fmt.Printf("Unmarshalled Request: %+v\n", request)
 	// fmt.Printf("Config: %+v\n", request.Config)
 
+	// ***DEBUG*** Print the Scope field of the Config object.
+	// fmt.Printf("Scope: %+v\n", request.Config.Scope)
+
 	// Convert the scopes in the configuration to assets.
-	Assets := convertScopeToAssets(testConf.Scope)
+	Assets := convertScopeToAssets(Request.Config.Scope)
+
+	// Initialize the event scheduler.
+	events.MainSchedulerInit()
 
 	// Iterate over each asset, marshal it to JSON, and unmarshal it back to an Asset object.
+	// for the client and the server, you still should run through the loop like i did here
+	// but only utilized the code that applies to the client or server.
+	i := 1
 	for _, asset := range Assets {
+		asset.Event = "asset#" + strconv.Itoa(i)
+		i++
 		assetJSON, err := json.Marshal(asset)
 		if err != nil {
 			log.Fatalf("Error occurred during marshalling: %s", err) // Handle marshalling errors.
 		}
-		assetString := string(assetJSON)
-		fmt.Println(assetString) // ***DEBUG*** PRINT THE ASSET JSON STRING
 
-		// ***DEBUG*** Unmarshal the asset JSON into an Asset object.
-		// unmarshalledAsset := &Asset{}
-		// err = json.Unmarshal(assetJSON, unmarshalledAsset)
-		// if err != nil {
-		// 	log.Fatalf("Error occurred during unmarshalling: %s", err) // Handle unmarshalling errors.
-		// }
+		// ***DEBUG*** PRINT THE ASSET JSON STRING
+		assetString := string(assetJSON)
+		fmt.Println("\n", assetString)
+
+		// Unmarshal the asset JSON into an Asset object.
+		unmarshalledAsset := &Asset{}
+		err = json.Unmarshal(assetJSON, unmarshalledAsset)
+		if err != nil {
+			log.Fatalf("Error occurred during unmarshalling: %s", err) // Handle unmarshalling errors.
+		}
 
 		// ***DEBUG*** Print the unmarshalled Asset object.
 		// fmt.Printf("Unmarshalled Asset: %+v\n", unmarshalledAsset)
+
+		event2Send := unmarshalledAsset.asset2Event()
+		// ***DEBUG*** Print the Event struct that was created from the Asset.
+		// fmt.Printf("Event: %+v\n", event2Send)
+
+		// Send the event to the event scheduler.
+		err = events.MainSchedulerSchedule(event2Send)
+		if err != nil {
+			log.Fatalf("Error occurred during scheduling: %s", err) // Handle scheduling errors.
+		}
+	}
+}
+
+// asset2Event converts an Asset to an Event. with the event name and data
+func (a *Asset) asset2Event() *events.Event {
+	return &events.Event{
+		Name: a.Event,
+		Data: a.Data,
+		Type: events.EventTypeSay,
 	}
 }
 
@@ -109,10 +152,14 @@ func convertScopeToAssets(scope *config.Scope) []*Asset {
 	// Convert Domains to assets.
 	for _, domain := range scope.Domains {
 		fqdn := fqdn.FQDN{Name: domain}
-		assets = append(assets, &Asset{
+		data := AssetData{
 			OAMAsset: fqdn,
 			OAMType:  fqdn.AssetType(),
-		})
+		}
+		asset := &Asset{
+			Data: data,
+		}
+		assets = append(assets, asset)
 	}
 
 	var ipType string
@@ -133,10 +180,11 @@ func convertScopeToAssets(scope *config.Scope) []*Asset {
 
 			// Create an asset from the IP address and append it to the assets slice.
 			asset := oamNet.IPAddress{Address: addr, Type: ipType}
-			assets = append(assets, &Asset{
+			data := AssetData{
 				OAMAsset: asset,
 				OAMType:  asset.AssetType(),
-			})
+			}
+			assets = append(assets, &Asset{Data: data})
 		}
 	}
 
@@ -156,19 +204,21 @@ func convertScopeToAssets(scope *config.Scope) []*Asset {
 
 		// Create an asset from the CIDR and append it to the assets slice.
 		asset := oamNet.Netblock{Cidr: prefix, Type: ipType}
-		assets = append(assets, &Asset{
+		data := AssetData{
 			OAMAsset: asset,
 			OAMType:  asset.AssetType(),
-		})
+		}
+		assets = append(assets, &Asset{Data: data})
 	}
 
 	// Convert ASNs to assets.
 	for _, asn := range scope.ASNs {
 		asset := oamNet.AutonomousSystem{Number: asn}
-		assets = append(assets, &Asset{
+		data := AssetData{
 			OAMAsset: asset,
 			OAMType:  asset.AssetType(),
-		})
+		}
+		assets = append(assets, &Asset{Data: data})
 	}
 
 	return assets
@@ -195,13 +245,125 @@ func UnmarshalRequest(jsonData string) (*Request, error) {
 	return &req, nil
 }
 
-// UnmarshalAsset unmarshals a JSON string into an Asset object.
-// THIS IS BROKEN MUST FIX LATER
-func UnmarshalAsset(jsonData string) (*Asset, error) {
-	var asset Asset
-	err := json.Unmarshal([]byte(jsonData), &asset)
-	if err != nil {
-		return nil, fmt.Errorf("error unmarshalling Asset: %w", err)
+// UnmarshalJSON is a custom unmarshaller for the Asset type, overriding the default JSON unmarshalling behavior.
+// This method is automatically invoked when 'json.Unmarshal' is called with an 'Asset' type (or pointer),
+// provided that the input JSON structure matches the Asset structure.
+//
+// The 'data' parameter holds the raw JSON data that is to be unmarshalled. This method's responsibility is to
+// process (parse and decode) this data and populate the 'Asset' struct's fields accordingly.
+//
+// If the method encounters any issue during unmarshalling that prevents successful completion,
+// it must return an error which will be propagated back through the call to 'json.Unmarshal'.
+//
+// This custom unmarshalling is particularly useful for handling complex scenarios during JSON decoding,
+// such as when the structure of JSON data doesn't map cleanly to the structure of the Go type,
+// or when some form of data transformation or validation is required during the unmarshalling process.
+//
+// Instead of having this here, we should implement this in the open-asset-model.
+// That way, others dont have to re-invent the wheel.
+func (d *AssetData) UnmarshalJSON(data []byte) error {
+	// First, unmarshal to a temporary struct to get the AssetType without unmarshalling the asset itself
+	var tmp struct {
+		OAMType  oam.AssetType   `json:"type"`
+		RawAsset json.RawMessage `json:"asset"` // Capture the asset as raw JSON
 	}
-	return &asset, nil
+	if err := json.Unmarshal(data, &tmp); err != nil {
+		return err
+	}
+
+	// Populate the known fields
+
+	d.OAMType = tmp.OAMType
+
+	// Based on the AssetType, we'll unmarshal the RawAsset into the appropriate struct
+	switch tmp.OAMType {
+	case oam.IPAddress:
+		var asset oamNet.IPAddress
+		if err := json.Unmarshal(tmp.RawAsset, &asset); err != nil {
+			return err
+		}
+		d.OAMAsset = asset
+
+	case oam.Netblock:
+		var asset oamNet.Netblock
+		if err := json.Unmarshal(tmp.RawAsset, &asset); err != nil {
+			return err
+		}
+		d.OAMAsset = asset
+
+	case oam.ASN:
+		var asset oamNet.AutonomousSystem
+		if err := json.Unmarshal(tmp.RawAsset, &asset); err != nil {
+			return err
+		}
+		d.OAMAsset = asset
+
+	case oam.RIROrg:
+		var asset oamNet.RIROrganization
+		if err := json.Unmarshal(tmp.RawAsset, &asset); err != nil {
+			return err
+		}
+		d.OAMAsset = asset
+
+	case oam.FQDN:
+		var asset fqdn.FQDN
+		if err := json.Unmarshal(tmp.RawAsset, &asset); err != nil {
+			return err
+		}
+		d.OAMAsset = asset
+
+	case oam.WHOIS:
+		var asset oamWHOIS.WHOIS
+		if err := json.Unmarshal(tmp.RawAsset, &asset); err != nil {
+			return err
+		}
+		d.OAMAsset = asset
+
+	case oam.Location:
+		var asset oamContact.Location
+		if err := json.Unmarshal(tmp.RawAsset, &asset); err != nil {
+			return err
+		}
+		d.OAMAsset = asset
+
+	case oam.Phone:
+		var asset oamContact.Phone
+		if err := json.Unmarshal(tmp.RawAsset, &asset); err != nil {
+			return err
+		}
+		d.OAMAsset = asset
+
+	case oam.Email:
+		var asset oamContact.EmailAddress
+		if err := json.Unmarshal(tmp.RawAsset, &asset); err != nil {
+			return err
+		}
+		d.OAMAsset = asset
+
+	case oam.Person:
+		var asset oamPeople.Person
+		if err := json.Unmarshal(tmp.RawAsset, &asset); err != nil {
+			return err
+		}
+		d.OAMAsset = asset
+
+	case oam.Organization, oam.Registrant:
+		var asset oamOrg.Organization
+		if err := json.Unmarshal(tmp.RawAsset, &asset); err != nil {
+			return err
+		}
+		d.OAMAsset = asset
+
+	case oam.Registrar:
+		var asset oamWHOIS.Registrar
+		if err := json.Unmarshal(tmp.RawAsset, &asset); err != nil {
+			return err
+		}
+		d.OAMAsset = asset
+
+	default:
+		return fmt.Errorf("unknown or unsupported asset type: %s", tmp.OAMType)
+	}
+
+	return nil
 }
